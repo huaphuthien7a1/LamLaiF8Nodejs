21c6baf389d9851844d213ceb0e549ebcf3a21af
huaphuthien7a1 <huaphuthienksst@gmail.com>
2022-05-04 21:27:53 +0700

    install husky

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
-const path = require('path');
-const router = require('./routes');
+const path = require("path");
+const router = require("./routes");
 //  HTTP logger
-const morgan = require('morgan');
-app.use(morgan('combined'));
+const morgan = require("morgan");
+app.use(morgan("combined"));
 
 // Template engine
-const handlebars = require('express-handlebars');
-const route = require('./routes');
-app.engine('.hbs', handlebars.engine({ extname: '.hbs' }));
-app.set('view engine', '.hbs');
-app.set('views', path.join(__dirname, '/resources/views'));
+const handlebars = require("express-handlebars");
+const route = require("./routes");
+app.engine(".hbs", handlebars.engine({ extname: ".hbs" }));
+app.set("view engine", ".hbs");
+app.set("views", path.join(__dirname, "/resources/views"));
 
 // Static file
-app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static(path.join(__dirname, "/public")));
 
 // Route init
 route(app);
